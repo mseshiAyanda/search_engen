161627385c20443cc7c57c27027f0e54bc411d4e
mseshiAyanda <mseshidladla@gmail.com>
2022-09-21 21:00:01 +0200

    second

--- search_engen/settings.py
+++ search_engen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kq7zlf6obo$xr*pr=rh(molheb0g1(!tnm#vx2hl035ny69mnz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
